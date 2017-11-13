ca49fac2853d886852e0f9dbb4de3730a143e538
lukas.lebo <lukas.lebo@hotmail.com>
2017-11-13 15:22:22 +0100

    Application prepared for Password Encrypton

--- yelp-rest-api/src/main/java/ch/propulsion/yelp/YelpRestApiApplication.java
+++ yelp-rest-api/src/main/java/ch/propulsion/yelp/YelpRestApiApplication.java
@@ -2,6 +2,8 @@ package ch.propulsion.yelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class YelpRestApiApplication {
@@ -9,4 +11,10 @@ public class YelpRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YelpRestApiApplication.class, args);
 	}
+	
+	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
 }
