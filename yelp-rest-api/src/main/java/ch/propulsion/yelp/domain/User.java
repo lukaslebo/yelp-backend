599bf44e5770fc3bbb3adf01040435c552151505
lukas.lebo <lukas.lebo@hotmail.com>
2017-11-14 21:19:28 +0100

    user entity implements spring security UserDetails

--- yelp-rest-api/src/main/java/ch/propulsion/yelp/domain/User.java
+++ yelp-rest-api/src/main/java/ch/propulsion/yelp/domain/User.java
@@ -1,19 +1,35 @@
 package ch.propulsion.yelp.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import ch.propulsion.yelp.repository.RoleRepository;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -58,23 +74,65 @@ public class User implements UserDetails {
 	@Column( unique = true, nullable = false )
 	private String email;
 	
-	@Column( nullable = false )
+	private String username;
+	
+	@Column( nullable = false, length = 100 )
 	private String password;
 	
 	@JsonView( JsonViews.Detail.class )
 	@OneToMany( mappedBy = "user", cascade = CascadeType.ALL )
 	private List<Review> reviews = new ArrayList<>();
+	
+	@ManyToMany( fetch = FetchType.EAGER )
+	@JoinTable( name = "user_roles" ) // , joinColumns = {@JoinColumn( name = "user_id", referencedColumnName = "id" )}, inverseJoinColumns = {@JoinColumn( name = "access_level_id", referencedColumnName = "id" )}
+	private Set<Role> roles = new HashSet<>();
 
 	public User(Long id, String firstName, String lastName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.username = email;
         this.password = password;
+        this.roles.add(roleRepository.findByName(Role.ROLE_USER));
     }
 	
 	public User(String firstName, String lastName, String email, String password) {
 		this(null, firstName, lastName, email, password);
+	}
+	
+	// -- Spring Security: UserDetails -----------------------------------------
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (getRoles() == null) {
+			return Collections.emptySet();
+		}
+		return getRoles().stream()
+				.map(Role::getName)
+				.map(String::toUpperCase)
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toSet());
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return isEnabled();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return isEnabled();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return isEnabled();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 }
