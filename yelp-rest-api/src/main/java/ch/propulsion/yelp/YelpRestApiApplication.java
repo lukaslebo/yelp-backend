4bd1951cb10f0ecd51d0b9ccf7ba92803150738d
lukas.lebo <lukas.lebo@hotmail.com>
2017-11-14 21:17:18 +0100

    moved BCrypt to security config

--- yelp-rest-api/src/main/java/ch/propulsion/yelp/YelpRestApiApplication.java
+++ yelp-rest-api/src/main/java/ch/propulsion/yelp/YelpRestApiApplication.java
@@ -2,8 +2,6 @@ package ch.propulsion.yelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class YelpRestApiApplication {
@@ -11,10 +9,5 @@ public class YelpRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YelpRestApiApplication.class, args);
 	}
-	
-	@Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 	
 }
